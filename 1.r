c28a40c9bc650f2b8d8b8901b8a788f12287e64c
HasanMansoorKhan <hasan.mansoor.1@hotmail.com>
2023-01-09 15:28:08 +0000

    Second Commit with solutions

--- 1.r
+++ 1.r
@@ -3,3 +3,102 @@ mds <- data.frame(mds)
 mds$car <- rownames(mtcars)
 ggplot(mds, aes(X1, X2, label = car)) + geom_text_repel()
 
+
+source('http://bit.ly/CEU-R-shoes')
+ls()
+str(students)
+plot(students$shoe, students$math)
+abline(lm(math ~ shoe, students), col = 'red')
+
+plot(students)
+
+library(GGally)
+ggpairs(students)
+
+rm(list=ls(all.names= TRUE))
+?ls
+ls(all.names = TRUE)
+?source
+readLines('http://bit.ly/CEU-R-shoes')
+?detach
+
+## 
+
+download.file('https://bit.ly/hun-cities-distance', 'cities.xls')
+
+library(readxl)
+cities <- read_excel('cities.xls')
+
+cities <- cities[, -1]
+cities <- cities[-nrow(cities), ]
+
+plot(cities)
+library(GGally)
+ggpairs(cities)
+
+?cmdscale
+mds <- cmdscale(as.dist(cities))
+plot(mds)
+text(mds[, 1], mds[, 2], names(cities))
+
+mds[, 1] <- -1 * mds[, 1]
+plot(mds)
+text(mds[, 1], mds[, 2], names(cities))
+
+## TODO redo with ggplot
+library(ggplot2)
+mds <- data.frame(mds)
+str(mds)
+mds$city <- names(cities)
+str(mds)
+g1 <- ggplot(mds, aes(X1, X2, label = city)) + geom_point() +
+  geom_text()
+
+g2 <- ggplot(mds, aes(X1, X2, label = city)) + geom_text()
+
+library(ggrepel)
+ggplot(mds, aes(X1, X2, label = city)) + geom_text_repel()
+
+?eurodist
+str(eurodist)
+
+
+mds <- cmdscale(eurodist)
+mds <- data.frame(mds)
+mds$city <- row.names(mds)
+ggplot(mds, aes(X1, -X2, label = city)) + geom_text_repel()
+
+## TODO same with mtcars
+?mtcars
+
+mds <- cmdscale(dist(mtcars))
+mds <- data.frame(mds)
+mds$car <- rownames(mtcars)
+ggplot(mds, aes(X1, X2, label = car)) + geom_text_repel()
+
+
+admission <- UCBAdmissions
+
+
+str(UCBAdmissions)
+admission <- as.data.frame(UCBAdmissions)
+
+
+library(ggplot2)
+
+## g3 <- ggplot(admission, aes(Gender, Freq, fill = Admit)) + facet_wrap(Dept ) + geom_col() + scale_fill_manual(values = c'admitted' = green )
+
+
+berkeley <- as.data.frame(UCBAdmissions)
+ggplot(berkeley, aes(Gender, Freq, fill = Admit)) + 
+  geom_col(position = 'fill') + scale_fill_brewer(palette = 'Dark2')
+
+ggplot(berkeley, aes(Gender, Freq, fill = Admit)) + 
+  geom_col(position = 'fill') + scale_fill_manual(values = c("Admitted" = 'darkgreen',
+                                                             'Rejected' = 'darkred'))
+
+ggplot(berkeley, aes(Gender, Freq, fill = Admit)) + 
+  facet_wrap(~Dept) + 
+  geom_col() + scale_fill_manual(values = c("Admitted" = 'darkgreen',
+                                            'Rejected' = 'darkred'))
+
